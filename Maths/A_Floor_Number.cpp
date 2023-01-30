9cf06303f489ab980976219dc34169c18a886376
Darshan <trivedidarshan30@gmail.com>
2023-01-30 21:20:34 +0530

    Maths Question CF - 30/01/2023

---
+++ Maths/A_Floor_Number.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+/*Keep learning
+Keep doing
+keep upsolving*/
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int n, x;
+    cin >> n >> x;
+    /*
+    1, 2
+    3 to (x + 2)
+    (x + 3) to (2*x+2)
+    (2*x+3) to (3*x + 2)
+    (3*x + 3) to (4*x + 2)
+    */
+    int start = 1, end = 2, floor = 1;
+    while (!(start <= n && end >= n))
+    {
+        int temp = start;
+        start = end + 1;
+        end = ((floor * x) + 2);
+        floor++;
+    }
+    pt(floor)
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
