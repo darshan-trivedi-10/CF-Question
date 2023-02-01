e0795a2dbd12f18c319e41aca464073761fcc897
Darshan <trivedidarshan30@gmail.com>
2023-02-01 14:15:43 +0530

    Maths - CF - 01/02/2023

---
+++ Maths/A_Three_Pairwise_Maximums.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+/*Keep learning
+Keep doing
+keep upsolving*/
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    if (x == y && x >= z)
+    {
+        ha
+        cout<< x << " " << z << " " << z << endl;
+    }
+    else if (y == z && y >= x)
+    {
+        ha
+        cout<< x << " " << x << " " << y << endl;
+    }
+    else if (x == z && x >= y)
+    {
+        ha
+        cout<< y << " " << x << " " << y << endl;
+    }
+    else
+    {
+        na
+    }
+
+    /*
+    x==y x > z,
+    y==z y > x,
+    x==z x>y
+    x=max(a,b)
+    y=max(a,c)
+    z=max(b,c)
+    */
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
